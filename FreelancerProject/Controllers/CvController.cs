d9885e0f19384d092b9400665b1f154e070c7459
sofievedo <sofie.vedo@gmail.com>
2019-10-30 10:47:31 +0100

    Lagt till Vymodell och controller för CV

---
+++ FreelancerProject/Controllers/CvController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FreelancerProject.Controllers
+{
+    public class CvController : Controller
+    {
+        // GET: Cv
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Cv/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Cv/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cv/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Cv/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Cv/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Cv/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Cv/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- FreelancerProject/ViewModels/CV.cs
+++ FreelancerProject/ViewModels/CV.cs
@@ -8,15 +8,72 @@ namespace FreelancerProject.ViewModels
 {
     public class CV
     {
-        public Education Education { get; set; }
+        private FreelancerEntities db = new FreelancerEntities();
+
+
+        #region Properties
+
+        public FreelancerPerson Freelancer { get; set; }
 
         private Competence competence;
 
         public Competence Competence
         {
             get { return competence; }
-            set { competence = value; }
+            set {
+                //Sätta kompetens baserat på ID?
+                competence = value; }
         }
+
+        private Education education;
+
+        public Education Education
+        {
+            get { return education; }
+            set { education = value; }
+        }
+
+        private OtherInfo otherInfo;
+
+        public OtherInfo OtherInfo
+        {
+            get { return otherInfo; }
+            set { otherInfo = value; }
+        }
+
+        private Work work;
+
+        public Work Work
+        {
+            get { return work; }
+            set { work = value; }
+        }
+
+        private Freelancer_Competence freelancer_Competence;
+
+        public Freelancer_Competence Freelancer_Competence
+        {
+            get { return freelancer_Competence; }
+            set { freelancer_Competence = value; }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public CV(int id)
+        {
+            this.Freelancer = db.FreelancerPerson.Find(id);
+        }
+
+
+
+        #endregion
+
+
+
+
+
 
     }
 }
