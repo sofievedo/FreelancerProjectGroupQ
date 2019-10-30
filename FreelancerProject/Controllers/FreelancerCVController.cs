36f69b6113cf4f1988c4490050efb6f9bc003204
sofievedo <sofie.vedo@gmail.com>
2019-10-30 18:47:47 +0100

    Påbörjat sida för CV. Skapat en vymodell

---
+++ FreelancerProject/Controllers/FreelancerCVController.cs
@@ -0,0 +1,24 @@
+﻿using FreelancerProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FreelancerProject.ViewModels;
+
+namespace FreelancerProject.Controllers
+{
+    public class FreelancerCVController : Controller
+    {
+        private FreelancerEntities db = new FreelancerEntities();
+
+        // GET: FreelancerCV
+        public ActionResult Index()
+        {
+            int id = 1; //TODO: Ta bort hårdkodning
+            var freelancer = new FreelancerCVViewmodel(id);
+
+            return View(freelancer);
+        }
+    }
+}
