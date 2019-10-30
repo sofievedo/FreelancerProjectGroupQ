83e35826019f024e6b5e0d533f74a7a693605bcb
elinsofiemadeleine <45717840+elinsofiemadeleine@users.noreply.github.com>
2019-10-30 15:09:25 +0100

    profil

--- FreelancerProject/Controllers/ProfileFreelancerController.cs
+++ FreelancerProject/Controllers/ProfileFreelancerController.cs
@@ -4,30 +4,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FreelancerProject.Models;
 
 namespace FreelancerProject.Controllers
 {
     public class ProfileFreelancerController : Controller
     {
-        private FreelancerPerson free = new FreelancerPerson();
 
+      
+        private FreelancerEntities db = new FreelancerEntities();
         // GET: ProfileFreelancer
         public ActionResult Index()
         {
+
+            var freelancer = db.FreelancerPerson.Find(1);
             return View();
         }
 
-        public ActionResult Add()
-        {
-            var firstname = free.Firstname;
-            var lastname = free.Lastname;
-            var address = free.Address;
-            var phone = free.Phonenumber;
-            var email = free.Email;
-            var link1 = free.LinkToGithub;
-            var link2 = free.LinkToLinkedIn;
-
-            return View();
-        }
+      
     }
 }
---
+++ FreelancerProject/Models/ProfileFreelancer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FreelancerProject.Models;
+
+
+namespace FreelancerProject.Models
+{
+    public class ProfileFreelancer
+    {
+    
+
+    }
+}
