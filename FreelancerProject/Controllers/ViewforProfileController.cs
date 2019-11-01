21d3c14d083266d91ec26ba1ae172f9afbd86476
elinsofiemadeleine <45717840+elinsofiemadeleine@users.noreply.github.com>
2019-11-01 11:04:37 +0100

    lagt in vyer för profil och cv

---
+++ FreelancerProject/Controllers/ViewforProfileController.cs
@@ -0,0 +1,23 @@
+﻿using FreelancerProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FreelancerProject.Controllers
+{
+    
+    public class ViewforProfileController : Controller
+    {
+        // GET: ViewforProfile
+
+        private FreelancerEntities db = new FreelancerEntities();
+
+        public ActionResult Index()
+        {
+            var freelancer = db.FreelancerPerson.Find(1);
+            return View(freelancer);
+        }
+    }
+}
