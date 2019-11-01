c7b464ee82c301e9d7f4db29e4342740f2b8b249
sofievedo <sofie.vedo@gmail.com>
2019-11-01 09:22:56 +0100

    Lagt till script och metoder för att välja kompetenser baserat på roll

---
+++ FreelancerProject/ViewModels/FreelancerCompetenceViewModel.cs
@@ -0,0 +1,18 @@
+﻿using FreelancerProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FreelancerProject.ViewModels
+{
+    public class FreelancerCompetenceViewModel
+    {
+        public int RoleId { get; set; }
+
+        public int CompetenceId { get; set; }
+
+        public ICollection<Role> Roles { get; set; }
+        public ICollection<Competence> Competences { get; set; }
+    }
+}
